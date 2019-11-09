60da8baf2155927cc03025390c604cbf80e9e411
Kalsyc <klassiclegend@gmail.com>
2019-11-09 17:00:10 +0800

    Add combined tests for Undo and Redo Commands

--- src/test/java/io/xpire/logic/commands/RedoCommandTest.java
+++ src/test/java/io/xpire/logic/commands/RedoCommandTest.java
@@ -1,27 +1,40 @@
 package io.xpire.logic.commands;
 
+import static io.xpire.logic.commands.CommandTestUtil.assertCommandFailure;
+import static io.xpire.logic.commands.RedoCommand.MESSAGE_REDO_FAILURE;
+import static io.xpire.testutil.TypicalItems.getTypicalLists;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.xpire.logic.commands.exceptions.CommandException;
+import io.xpire.logic.parser.exceptions.ParseException;
+import io.xpire.model.Model;
+import io.xpire.model.ModelManager;
+import io.xpire.model.UserPrefs;
+import io.xpire.model.state.StackManager;
+import io.xpire.model.state.StateManager;
+
+/**
+ * Test for Redo Command. Since commands that are not undoable are also not redoable, we only test if there
+ * are no more Redoable Commands
+ */
 public class RedoCommandTest {
 
+    private Model model;
+    private StateManager stateManager;
 
-    //Testing Redo for AddCommand
-    //Testing Redo for CheckCommand
-    //Testing Redo for ClearCommand
-    //Testing Redo for DeleteCommand(Item)
-    //Testing Redo for DeleteCommand(Tags)
-    //Testing Redo for DeleteCommand(Quantity)
-    //Testing Redo for DeleteCommand(Quantity + Shift)
-    //Testing Redo for SearchCommand
-    //Testing Redo for SetReminderCommand
-    //Testing Redo for ShiftToMainCommand
-    //Testing Redo for ShiftToReplenishCommand
-    //Testing Redo for SortCommand
-    //Testing Redo for TagCommand
-    //Testing Redo for ViewCommand
-    //Testing Redo for Multiple Commands
-    //Testing Redo for RedoCommand(Should not Undo)
-    //Testing Redo for TagCommand(Show)(Should not Undo)
-    //Testing Redo for ExitCommand(Should not Redo)
-    //Testing Redo for HelpCommand(Should not Redo)
-    //Testing Redo for ExportCommand(Should not Redo)
+    @BeforeEach
+    public void setUp() throws CommandException, ParseException {
+        model = new ModelManager(getTypicalLists(), new UserPrefs());
+        stateManager = new StackManager();
+
+    }
+
     //Testing Redo if there are no Redoable States
+    @Test
+    public void execute_noRedoableStates_throwsCommandException() throws CommandException, ParseException {
+        RedoCommand redoCommand = new RedoCommand();
+        assertCommandFailure(redoCommand, model, MESSAGE_REDO_FAILURE);
+    }
 }
